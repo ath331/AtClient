2ad695d77ace9e339d2e38493aee9a731968a026
ath331 <kstnftk@naver.com>
2024-01-24 22:25:59 +0900

    프로토 파일 추가
    - ProtoBufCore라고 하면 안되고
    ProtobufCore라고 하면 되네.. 언리얼에서 기본적으로 제공해주는 프로토버퍼랑 겹치나봄

--- Source/AtClient/AtClient.Build.cs
+++ Source/AtClient/AtClient.Build.cs
@@ -10,7 +10,7 @@ public class AtClient : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "ProtoBufCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "ProtobufCore" });
 
 		PrivateIncludePaths.AddRange( new string[]
 		{
