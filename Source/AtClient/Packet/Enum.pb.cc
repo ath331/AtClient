3d9db9403911e271e7dd0eb48ff85e4a8a81d37f
ath331 <kstnftk@naver.com>
2024-09-08 00:42:01 +0900

    Enum 최신화

--- Source/AtClient/Packet/Enum.pb.cc
+++ Source/AtClient/Packet/Enum.pb.cc
@@ -18,29 +18,30 @@
 PROTOBUF_PRAGMA_INIT_SEG
 namespace Protocol {
 }  // namespace Protocol
-static const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* file_level_enum_descriptors_Enum_2eproto[4];
+static const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* file_level_enum_descriptors_Enum_2eproto[5];
 static constexpr ::PROTOBUF_NAMESPACE_ID::ServiceDescriptor const** file_level_service_descriptors_Enum_2eproto = nullptr;
 const ::PROTOBUF_NAMESPACE_ID::uint32 TableStruct_Enum_2eproto::offsets[1] = {};
 static constexpr ::PROTOBUF_NAMESPACE_ID::internal::MigrationSchema* schemas = nullptr;
 static constexpr ::PROTOBUF_NAMESPACE_ID::Message* const* file_default_instances = nullptr;
 
 const char descriptor_table_protodef_Enum_2eproto[] PROTOBUF_SECTION_VARIABLE(protodesc_cold) =
-  "\n\nEnum.proto\022\010Protocol*j\n\nObjectType\022\024\n\020"
-  "OBJECT_TYPE_NONE\020\000\022\025\n\021OBJECT_TYPE_ACTOR\020"
-  "\001\022\032\n\026OBJECT_TYPE_PROJECTILE\020\002\022\023\n\017OBJECT_"
-  "TYPE_ENV\020\003*c\n\tActorType\022\023\n\017Actor_TYPE_NO"
-  "NE\020\000\022\025\n\021Actor_TYPE_PLAYER\020\001\022\026\n\022Actor_TYP"
-  "E_MONSTER\020\002\022\022\n\016Actor_TYPE_NPC\020\003*h\n\nPlaye"
-  "rType\022\024\n\020PLAYER_TYPE_NONE\020\000\022\026\n\022PLAYER_TY"
-  "PE_KNIGHT\020\001\022\024\n\020PLAYER_TYPE_MAGE\020\002\022\026\n\022PLA"
-  "YER_TYPE_ARCHER\020\003*t\n\tMoveState\022\023\n\017MOVE_S"
-  "TATE_NONE\020\000\022\023\n\017MOVE_STATE_IDLE\020\001\022\022\n\016MOVE"
-  "_STATE_RUN\020\002\022\023\n\017MOVE_STATE_JUMP\020\003\022\024\n\020MOV"
-  "E_STATE_SKILL\020\004b\006proto3"
+  "\n\nEnum.proto\022\010Protocol*k\n\013EObjectType\022\024\n"
+  "\020OBJECT_TYPE_NONE\020\000\022\025\n\021OBJECT_TYPE_ACTOR"
+  "\020\001\022\032\n\026OBJECT_TYPE_PROJECTILE\020\002\022\023\n\017OBJECT"
+  "_TYPE_ENV\020\003*8\n\nEActorType\022\010\n\004None\020\000\022\n\n\006P"
+  "layer\020\001\022\013\n\007Monster\020\002\022\007\n\003Npc\020\003*P\n\010EBagTyp"
+  "e\022\017\n\013BagTypeNone\020\000\022\r\n\tEquipment\020\001\022\007\n\003Etc"
+  "\020\002\022\013\n\007Useable\020\003\022\016\n\nBagTypeMax\020\004*i\n\013EPlay"
+  "erType\022\024\n\020PLAYER_TYPE_NONE\020\000\022\026\n\022PLAYER_T"
+  "YPE_KNIGHT\020\001\022\024\n\020PLAYER_TYPE_MAGE\020\002\022\026\n\022PL"
+  "AYER_TYPE_ARCHER\020\003*u\n\nEMoveState\022\023\n\017MOVE"
+  "_STATE_NONE\020\000\022\023\n\017MOVE_STATE_IDLE\020\001\022\022\n\016MO"
+  "VE_STATE_RUN\020\002\022\023\n\017MOVE_STATE_JUMP\020\003\022\024\n\020M"
+  "OVE_STATE_SKILL\020\004b\006proto3"
   ;
 static ::PROTOBUF_NAMESPACE_ID::internal::once_flag descriptor_table_Enum_2eproto_once;
 const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable descriptor_table_Enum_2eproto = {
-  false, false, 463, descriptor_table_protodef_Enum_2eproto, "Enum.proto", 
+  false, false, 505, descriptor_table_protodef_Enum_2eproto, "Enum.proto", 
   &descriptor_table_Enum_2eproto_once, nullptr, 0, 0,
   schemas, file_default_instances, TableStruct_Enum_2eproto::offsets,
   nullptr, file_level_enum_descriptors_Enum_2eproto, file_level_service_descriptors_Enum_2eproto,
@@ -52,11 +53,11 @@ PROTOBUF_ATTRIBUTE_WEAK const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable
 // Force running AddDescriptors() at dynamic initialization time.
 PROTOBUF_ATTRIBUTE_INIT_PRIORITY static ::PROTOBUF_NAMESPACE_ID::internal::AddDescriptorsRunner dynamic_init_dummy_Enum_2eproto(&descriptor_table_Enum_2eproto);
 namespace Protocol {
-const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* ObjectType_descriptor() {
+const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* EObjectType_descriptor() {
   ::PROTOBUF_NAMESPACE_ID::internal::AssignDescriptors(&descriptor_table_Enum_2eproto);
   return file_level_enum_descriptors_Enum_2eproto[0];
 }
-bool ObjectType_IsValid(int value) {
+bool EObjectType_IsValid(int value) {
   switch (value) {
     case 0:
     case 1:
@@ -68,11 +69,11 @@ bool ObjectType_IsValid(int value) {
   }
 }
 
-const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* ActorType_descriptor() {
+const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* EActorType_descriptor() {
   ::PROTOBUF_NAMESPACE_ID::internal::AssignDescriptors(&descriptor_table_Enum_2eproto);
   return file_level_enum_descriptors_Enum_2eproto[1];
 }
-bool ActorType_IsValid(int value) {
+bool EActorType_IsValid(int value) {
   switch (value) {
     case 0:
     case 1:
@@ -84,11 +85,28 @@ bool ActorType_IsValid(int value) {
   }
 }
 
-const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* PlayerType_descriptor() {
+const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* EBagType_descriptor() {
   ::PROTOBUF_NAMESPACE_ID::internal::AssignDescriptors(&descriptor_table_Enum_2eproto);
   return file_level_enum_descriptors_Enum_2eproto[2];
 }
-bool PlayerType_IsValid(int value) {
+bool EBagType_IsValid(int value) {
+  switch (value) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      return true;
+    default:
+      return false;
+  }
+}
+
+const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* EPlayerType_descriptor() {
+  ::PROTOBUF_NAMESPACE_ID::internal::AssignDescriptors(&descriptor_table_Enum_2eproto);
+  return file_level_enum_descriptors_Enum_2eproto[3];
+}
+bool EPlayerType_IsValid(int value) {
   switch (value) {
     case 0:
     case 1:
@@ -100,11 +118,11 @@ bool PlayerType_IsValid(int value) {
   }
 }
 
-const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* MoveState_descriptor() {
+const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* EMoveState_descriptor() {
   ::PROTOBUF_NAMESPACE_ID::internal::AssignDescriptors(&descriptor_table_Enum_2eproto);
-  return file_level_enum_descriptors_Enum_2eproto[3];
+  return file_level_enum_descriptors_Enum_2eproto[4];
 }
-bool MoveState_IsValid(int value) {
+bool EMoveState_IsValid(int value) {
   switch (value) {
     case 0:
     case 1:
